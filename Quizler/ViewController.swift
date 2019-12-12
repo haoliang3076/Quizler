e8a27cb52bca8a4ecfa5fcf98a003e8138e9f9db
Haoliang Zhang <haoliang3076@gmail.com>
2019-12-12 17:29:42 -0500

    changes1

--- Quizler/ViewController.swift
+++ Quizler/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var QuestionLabel: UILabel!
+    
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    @IBOutlet weak var Button3: UIButton!
+    @IBOutlet weak var Button4: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    func RandomQuestion(){
+        var RandomNumber = arc4random()%4
+        RandomNumber += 1
+    }
 
-
+    @IBAction func Button1Action(_ sender: Any) {
+    }
+    
+    @IBAction func Button2Action(_ sender: Any) {
+    }
+    @IBAction func Button3Action(_ sender: Any) {
+    }
+    @IBAction func Button4Action(_ sender: Any) {
+    }
 }
 
